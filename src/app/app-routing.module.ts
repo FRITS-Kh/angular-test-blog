f329a9952d75ff67cec27a5b21554fcceb25d234
Yuriy Gerton <yuriy.gerton@spryker.com>
2022-05-05 12:58:54 +0300

    introduce base admin panel structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,11 @@ const routes: Routes = [
       },
     ],
   },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((module) => module.AdminModule),
+  },
 ];
 
 @NgModule({
